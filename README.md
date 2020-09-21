91825eef5cfb922cfd038710c78f5868104f80ba
pratik <pratikroy311@gmail.com>
2020-09-21 16:36:17 +0530

    verison 1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# review_analyser
+
+## Currently in development stage
+
+### Currently Sentiment is predicted using Decision Tree Classifier
+#### Will be updating with more models
+
+## Dependencies:
+* python3.7
+* flask
+* scikit learn
+* BeautifulSoup
+
+## Installation:
+* install python3.7
+* install pip
+* create a environment(Better if you install conda and create a seperate environment for all ML projects)
+* pip install scikit-learn
+* pip install flask
+* move to project directory and open terminal
+* run command ```python manage.py runserver```
---
+++ main.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from joblib import dump,load
+from bs4 import BeautifulSoup
+import re
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+###################################################
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+
+stopwords= set(['br', 'the', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't"])
+
+
+def clean_text(sentence):
+    sentence = re.sub(r"http\S+", "", sentence)
+    sentence = BeautifulSoup(sentence, 'lxml').get_text()
+    sentence = decontracted(sentence)
+    sentence = re.sub("\S*\d\S*", "", sentence).strip()
+    sentence = re.sub('[^A-Za-z]+', ' ', sentence)
+    # https://gist.github.com/sebleier/554280
+    sentence = ' '.join(e.lower() for e in sentence.split() if e.lower() not in stopwords)
+    return sentence.strip()
+###################################################
+clf = load('dt_model.joblib')
+count_vect = load('tfidf_vect.joblib')
+def predict(string):
+    review_text = clean_text(string)
+    test_vect = count_vect.transform(([review_text]))
+    pred = clf.predict(test_vect)
+    print(pred[0])
+    if pred[0]:
+        prediction = "Positive"
+    else:
+        prediction = "Negative"
+    return prediction
